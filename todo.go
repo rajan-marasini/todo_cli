fbbe295c7c56852c866fcaffafc5efcec2e9e05c
Rajan Marasini <official.rajan2060@gmail.com>
2025-05-18 19:46:54 +0545

    delete method

--- todo.go
+++ todo.go
@@ -27,9 +27,21 @@ func (todos *Todos) add(title string) {
 
 func (todos *Todos) validateIndex(index int) error {
 	if index < 0 || index >= len(*todos) {
-		err := errors.New("Invalid index")
+		err := errors.New("invalid index")
 		fmt.Println(err)
 		return err
 	}
+	return nil
+}
+
+func (todos *Todos) delete(index int) error {
+	t := *todos
+
+	if err := t.validateIndex(index); err != nil {
+		return err
+	}
+
+	*todos = append(t[:index], t[index+1:]...)
+
 	return nil
 }
