9f8d429eefccde041230523ede4c48705b42d9f6
Rajan Marasini <official.rajan2060@gmail.com>
2025-05-18 20:11:49 +0545

    implemented storage in json file

--- main.go
+++ main.go
@@ -1,14 +1,9 @@
 package main
 
-import "fmt"
-
 func main() {
 	todos := Todos{}
-	todos.add("Buy Milk")
-	todos.add("Buy bread")
-	todos.add("Buy rice")
-	fmt.Printf("%+v\n\n", todos)
-	todos.delete(0)
-	todos.toggle(0)
+	storage := NewStorage[Todos]("todos.json")
+	storage.Load(&todos)
 	todos.print()
+	storage.Save(todos)
 }
