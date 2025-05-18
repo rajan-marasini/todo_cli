dc9bdd22088c667a51c8106df83de5fff593b297
Rajan Marasini <official.rajan2060@gmail.com>
2025-05-18 19:53:20 +0545

    edit method

--- todo.go
+++ todo.go
@@ -63,5 +63,16 @@ func (todos *Todos) toggle(index int) error {
 	t[index].Completed = !isCompleted
 
 	return nil
+}
 
+func (todos *Todos) edit(index int, title string) error {
+	t := *todos
+
+	if err := t.validateIndex(index); err != nil {
+		return err
+	}
+
+	t[index].Title = title
+
+	return nil
 }
